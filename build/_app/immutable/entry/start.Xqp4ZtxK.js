071464ed05e87f8b89a495c955cf98b1291db9a0
devnull03 <56480041+devnull03@users.noreply.github.com>
2024-01-30 12:07:38 -0800

    feat: build changes

--- build/_app/immutable/entry/start.Xqp4ZtxK.js
+++
@@ -1,1 +0,0 @@
-import{a as t}from"../chunks/entry.UWIzW4QM.js";export{t as start};
---
+++ build/_app/immutable/nodes/2.TqszdBRn.js
@@ -0,0 +1,33 @@
+import{s as o,n as i}from"../chunks/scheduler.dtLwcIix.js";import{S as l,i as r,e as c,c as d,g as h,a as u,b as m,d as f}from"../chunks/index.M7MzkmBA.js";function p(s){let t,a=`<div class="bg-[#343A40] bg-cover bg-center w-full" style="background-image: url(https://www.southasiancanadianheritage.ca/wp-content/uploads/2024/01/img4.jpg);"><h1 class="text-4xl text-white font-semibold bg-[#1C375E] bg-opacity-50 w-full h-full py-60 flex items-center justify-center">South Asian Discriminatory Laws</h1></div> <div class="flex items-center justify-center py-28 bg-[#f8f9fa] w-full"><iframe src="https://cdn.knightlab.com/libs/timeline3/latest/embed/index.html?source=132DfhxF5iJTrjHiVMfGkVyMojIg2gbqaViMSO1Uu_kY&amp;font=Default&amp;lang=en&amp;initial_zoom=2&amp;height=650" width="85%" height="650" webkitallowfullscreen="" mozallowfullscreen="" allowfullscreen="" frameborder="0" title="Timeline"></iframe></div> <div class="flex flex-col gap-2 w-[80%] py-20"><h2 class="text-4xl font-medium">About</h2> <p class="text-xl font-light">Canada, known for its multicultural society and commitment to human
+      rights, has a history marked by discriminatory laws, policies, and
+      practices targeted at various communities. This platform aims to shed
+      light on the development of racist laws and practices against South Asians
+      in Canada. Exploring the historical records, we seek to illuminate the
+      adversities, obstacles, and victories of the South Asian community in
+      dealing with systemic prejudice.</p></div> <section><div class="flex flex-row-reverse"><img src="https://www.southasiancanadianheritage.ca/wp-content/uploads/2024/01/img1.jpg" alt="" class="w-1/2 h-auto"/> <div class="flex flex-col items-center justify-center gap-4 p-28"><h2 class="text-4xl font-medium">Immigration Act 1906 and continuous journey Act 1908</h2> <p class="text-xl font-light">The Immigration Act of 1906 marked a significant shift towards
+          stricter immigration policies. It broadened the list of forbidden
+          immigrant categories, established a formalized deportation procedure,
+          and granted the government expanded authority to exercise
+          discretionary judgments regarding admission.<br/><br/>
+          An amendment to the Immigration Act was made in 1908 to incorporate the
+          &quot;continuous journey regulation&quot; of the Canadian government, which forbade
+          the entry into Canada of any immigrant who had not traveled continuously
+          from the place of their birth or citizenship. As most voyages from the
+          region involved stops, this posed a considerable obstacle to immigrants
+          from South Asia.</p></div></div> <div class="flex"><img src="https://www.southasiancanadianheritage.ca/wp-content/uploads/2024/01/img2.jpg" alt="" class="w-1/2 h-auto"/> <div class="flex flex-col items-center justify-center gap-4 p-28"><h2 class="text-4xl font-medium w-full">Dominion Elections Act</h2> <p class="text-xl font-light">In 1920, the Dominion Elections Act resulted in the removal of federal
+          voting privileges for those who had been denied the right to vote at
+          the provincial level due to their ethnicity, particularly impacting
+          people of Chinese, Japanese, and South Asian descent in British
+          Columbia. Moreover, individuals of Chinese origin in Saskatchewan were
+          also denied their right to vote.</p></div></div> <div class="flex flex-row-reverse w-full"><img src="https://www.southasiancanadianheritage.ca/wp-content/uploads/2024/01/img3.jpg" alt="" class="w-1/2 h-au"/> <div class="flex flex-col items-center justify-center gap-4 p-28"><h2 class="text-4xl font-medium w-full">The Komagata Maru Incident</h2> <p class="text-xl font-light">This incident unfolded during the spring and summer of 1914, just
+          before the onset of the First World War. It turned out to be a
+          harrowing and sorrowful ordeal for the passengers involved. Initially,
+          there was an unsuccessful attempt at confrontation with officials,
+          police, and the military at the Port of Vancouver. Subsequently, as
+          the passengers returned to India, they faced a tragic and fatal clash
+          with police and troops near Kolkata. The Komagata Maru tale
+          illustrates the Canadian government&#39;s purposeful exclusionary policy
+          toward ethnic groups it deemed unfit to enter. These defenses of
+          Canada&#39;s continued status as a &quot;White Man&#39;s Country&quot; were couched in
+          racist and ethnocentric notions of &quot;progress,&quot; &quot;civilization,&quot; and
+          &quot;suitability.&quot;</p></div></div></section>`;return{c(){t=c("main"),t.innerHTML=a,this.h()},l(e){t=d(e,"MAIN",{class:!0,"data-svelte-h":!0}),h(t)!=="svelte-1veup9a"&&(t.innerHTML=a),this.h()},h(){u(t,"class","flex flex-col items-center mb-4")},m(e,n){m(e,t,n)},p:i,i,o:i,d(e){e&&f(t)}}}class v extends l{constructor(t){super(),r(this,t,null,p,o,{})}}export{v as component};
