2867df29cd3926e8ef13b93cdd7fa0ee16babcf0
devnull03 <56480041+devnull03@users.noreply.github.com>
2024-02-13 12:42:52 -0800

    background texture

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -7,15 +7,6 @@
 
 <svelte:head>
   <title>South Asian Discriminatory Laws</title>
-
-  <link
-    href="https://fonts.googleapis.com/css?family=Lato:300,400,700,300italic,400italic,700italic"
-    rel="stylesheet"
-    type="text/css"
-  />
-  <!-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"> -->
-  <script src="https://kit.fontawesome.com/30f055fc02.js" crossorigin="anonymous"></script>
-
 </svelte:head>
 
 <Header />
