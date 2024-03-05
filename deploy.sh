dabb80414966fcf323be02ab247eb222a27470ea
devnull03 <56480041+devnull03@users.noreply.github.com>
2024-03-05 15:07:18 -0800

    deploy script fix

--- deploy.sh
+++ deploy.sh
@@ -4,4 +4,4 @@ git init
 git add -A
 git commit -m 'deploy'
 
-git push -f git@github.com:south-asian-canadian-digital-archive/legal-history-page.git master:build
+git push -f git@github.com:south-asian-canadian-digital-archive/legal-history-page.git main:build
