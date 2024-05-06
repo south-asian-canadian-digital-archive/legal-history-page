d7564ac8333cc539cad628068c71954bd2c9f96d
devnull03 <56480041+devnull03@users.noreply.github.com>
2024-05-06 21:31:43 +0530

    stuff

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -11,22 +11,24 @@
   </ParallaxLayer>
 
   <ParallaxLayer offset={0} rate={1.3}>
-    <div id="title" class="h-screen flex justify-center items-end bg-[#5353537f]">
+    <div
+      id="title"
+      class="h-screen flex justify-center items-end bg-[#5353537f]"
+    >
       <h1
         class="lg:text-4xl md:text-6xl text-3xl text-[#ffffff] px-[15%] lg:pb-16 md:pb-16 pt-36 font-semibold flex flex-col gap-4 w-full items-center text-center font-mono"
       >
         Timeline of Discriminatory Laws: A South Asian History
         <span class="lg:text-base md:text-base text-sm font-normal px-[10%]">
-Exploring the historical records, we seek to
-          illuminate the adversities, obstacles, and victories of the South
-          Asian community in dealing with systemic prejudice.
+          Exploring the historical records, we seek to illuminate the
+          adversities, obstacles, and victories of the South Asian community in
+          dealing with systemic prejudice.
         </span>
       </h1>
     </div>
   </ParallaxLayer>
 
   <ParallaxLayer offset={1} rate={0.1}>
-
     <div
       id="about"
       class="flex lg:flex-row md:flex-row flex-col items-center justify-center h-screen w-full lg:pl-20 md:pl-20 lg:p-0 md:p-0 p-10 gap-10 bg-[#f3f3f3]"
@@ -38,7 +40,7 @@ Exploring the historical records, we seek to
           rights, has a history marked by discriminatory laws, policies, and
           practices targeted at various communities. This platform aims to shed
           light on the development of racist laws and practices against South
-          Asians in Canada. 
+          Asians in Canada.
         </p>
       </div>
 
@@ -48,7 +50,9 @@ Exploring the historical records, we seek to
     </div>
 
     <div class="flex flex-col items-center h-screen justify-center pt-20">
-      <h2 class="text-4xl font-semibold border-b-2 pb-6 w-[80%] text-center font-mono">
+      <h2
+        class="text-4xl font-semibold border-b-2 pb-6 w-[80%] text-center font-mono"
+      >
         A Timeline of Discriminatory laws
       </h2>
 
